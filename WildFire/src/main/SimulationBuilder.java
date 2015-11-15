24d6dbb15ac494c9b5fdc6dcd6a9e44053df677c
Carsten Orth <carsten_orth1@hotmail.de>
2015-11-15 15:32:51 +0100

    dynamic parameters

--- WildFire/src/main/SimulationBuilder.java
+++ WildFire/src/main/SimulationBuilder.java
@@ -12,6 +12,8 @@ import repast.simphony.context.space.continuous.ContinuousSpaceFactoryFinder;
 import repast.simphony.context.space.grid.GridFactory;
 import repast.simphony.context.space.grid.GridFactoryFinder;
 import repast.simphony.dataLoader.ContextBuilder;
+import repast.simphony.engine.environment.RunEnvironment;
+import repast.simphony.parameter.Parameters;
 import repast.simphony.space.continuous.ContinuousSpace;
 import repast.simphony.space.continuous.NdPoint;
 import repast.simphony.space.continuous.RandomCartesianAdder;
@@ -21,17 +23,30 @@ import repast.simphony.space.grid.SimpleGridAdder;
 import repast.simphony.space.grid.StrictBorders;
 
 public class SimulationBuilder implements ContextBuilder<Object>{
-	public static final int FOREST_DIM = 40;
 	
-	public static final int MAX_CLOUD_DIM = 8;
-	public static final int MIN_CLOUD_DIM = 3;
-	public static final double CLOUD_FREQUENCY = 2;
-	
-	public static final double MAX_WIND_SPEED = 0.8;
+
 	
 	
 	@Override
 	public Context build(Context<Object> context) {
+		Parameters  params = RunEnvironment.getInstance (). getParameters ();
+		
+		int forestDim = (Integer)params.getValue("forest_dim");
+		
+		double maxWindSpeed = (Double)params.getValue("max_wind_speed");
+		int maxCloudDim = (Integer)params.getValue("max_cloud_dim");
+		int minCloudDim = (Integer)params.getValue("min_cloud_dim");
+		double cloudRate = (Double)params.getValue("cloud_rate");
+		double maxWoodHealth = (Double)params.getValue("max_wood_health");
+		double minWoodHealth = (Double)params.getValue("min_wood_health");
+		double maxMaterialFactor = (Double)params.getValue("max_material_factor");
+		double minMaterialFactor = (Double)params.getValue("min_material_factor");
+		double sparkingFactor = (Double)params.getValue("sparking_factor");
+		double maxCloudTank = (Double)params.getValue("max_cloud_tank");
+		double minCloudTank = (Double)params.getValue("min_cloud_tank");
+		double maxRain = (Double)params.getValue("max_rain");
+		double minRain = (Double)params.getValue("min_rain");
+		
 		//id of this context
 		context.setId("WildFire");
 		
@@ -39,14 +54,14 @@ public class SimulationBuilder implements ContextBuilder<Object>{
 		RandomCartesianAdder<Object> terrainAdder = new RandomCartesianAdder<Object>(); //adder who places objects in terrain
 		
 		ContinuousSpaceFactory spaceFactory = ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null);
-		ContinuousSpace<Object> space = spaceFactory.createContinuousSpace("space", context, terrainAdder , new repast.simphony.space.continuous.StrictBorders(), FOREST_DIM+2*MAX_CLOUD_DIM, FOREST_DIM+2*MAX_CLOUD_DIM);
+		ContinuousSpace<Object> space = spaceFactory.createContinuousSpace("space", context, terrainAdder , new repast.simphony.space.continuous.StrictBorders(), forestDim+2*maxCloudDim, forestDim+2*maxCloudDim);
 		
 		//create grid (to use for neighbourhood)
 		SimpleGridAdder<Object> gridAdder = new SimpleGridAdder<Object>(); //adder who places objects in grid
 		
 		GridFactory gridFactory = GridFactoryFinder.createGridFactory(null);
 		//the boolean determines if more than one object can occupy a gridcell
-		Grid<Object> grid = gridFactory.createGrid("grid", context, new GridBuilderParameters <Object >(new StrictBorders(), gridAdder, true, FOREST_DIM+2*MAX_CLOUD_DIM, FOREST_DIM+2*MAX_CLOUD_DIM));
+		Grid<Object> grid = gridFactory.createGrid("grid", context, new GridBuilderParameters <Object >(new StrictBorders(), gridAdder, true, forestDim+2*maxCloudDim, forestDim+2*maxCloudDim));
 		
 		
 		//-------------------------------------------------
@@ -54,17 +69,17 @@ public class SimulationBuilder implements ContextBuilder<Object>{
 		//-------------------------------------------------
 		
 		//wind (simulates global wind-speed and -direction)
-		Wind wind = new Wind(MAX_WIND_SPEED);
+		Wind wind = new Wind(maxWindSpeed);
 		context.add(wind);
 		
 		//CloudFactory (creates clouds, which enter the map with respect to the actual wind-direction)
-		CloudFactory cf = new CloudFactory(context, wind, space, grid, CLOUD_FREQUENCY, MIN_CLOUD_DIM, MAX_CLOUD_DIM);
+		CloudFactory cf = new CloudFactory(context, wind, space, grid, cloudRate, minCloudDim, maxCloudDim, forestDim, maxRain, minRain, maxCloudTank, minCloudTank);
 		context.add(cf);
 		
 		//add forrest
-		ForestFactory forrest = new ForestFactory(1500, 800, 0.2, 0.05);
-		for(int i = MAX_CLOUD_DIM;i<MAX_CLOUD_DIM+FOREST_DIM;i++){
-			for(int j = MAX_CLOUD_DIM;j<MAX_CLOUD_DIM+FOREST_DIM;j++){
+		ForestFactory forrest = new ForestFactory(maxWoodHealth, minWoodHealth, maxMaterialFactor, minMaterialFactor);
+		for(int i = maxCloudDim;i<maxCloudDim+forestDim;i++){
+			for(int j = maxCloudDim;j<maxCloudDim+forestDim;j++){
 				Wood w = forrest.getWood();
 				context.add(w);
 				space.moveTo(w, i, j);
@@ -76,7 +91,7 @@ public class SimulationBuilder implements ContextBuilder<Object>{
 			context.add(new SimpleAgent(space, grid, 1, 2));
 		}
 		
-		FireFactory fire = new FireFactory(context, grid, space, wind, 0.05);
+		FireFactory fire = new FireFactory(context, grid, space, wind, sparkingFactor, forestDim, maxCloudDim);
 		context.add(fire);
 		
 		//-------------------------------------
