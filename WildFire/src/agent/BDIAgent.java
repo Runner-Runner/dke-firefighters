13d5f76c5c1afd357238124ff8652b43b6ffe230
Carsten Orth <carsten_orth1@hotmail.de>
2015-11-22 19:28:01 +0100

    bdi agent request logic

--- WildFire/src/agent/BDIAgent.java
+++ WildFire/src/agent/BDIAgent.java
@@ -3,8 +3,10 @@ package agent;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Random;
 
+import repast.simphony.engine.environment.RunEnvironment;
 import repast.simphony.query.space.grid.GridCell;
 import repast.simphony.query.space.grid.GridCellNgh;
 import repast.simphony.space.continuous.ContinuousSpace;
@@ -16,22 +18,24 @@ import agent.communication.info.Information;
 import agent.communication.request.ActionRequest;
 import agent.communication.request.InformationRequest;
 import agent.communication.request.Request;
+import agent.communication.request.RequestConfirm;
+import agent.communication.request.RequestDismiss;
 import agent.communication.request.RequestOffer;
 import environment.Fire;
 import environment.Wood;
 
 public class BDIAgent extends ForesterAgent{
-	private int increasingRange;
-	private List<Request> myRequest;
-	private HashMap<String, String> requestedAgents; //agentID -> requestID
+	private HashMap<Request, Integer> openRequests; //request -> range
+	private HashMap<Integer, Request> openIDs; //requestID -> request
+	private HashMap<String, Request> requestedAgents; //agentID -> request
 	
 	
 	public BDIAgent(ContinuousSpace<Object> space, Grid<Object> grid,
 			double speed, double extinguishRate) {
 		super(space, grid, speed, extinguishRate);
-		this.increasingRange = 3;
-		this.myRequest = new LinkedList<Request>();
-		this.requestedAgents = new HashMap<String, String>();
+		this.openRequests = new HashMap<Request, Integer>();
+		this.openIDs = new HashMap<Integer, Request>();
+		this.requestedAgents = new HashMap<String, Request>();
 	}
 
 
@@ -79,29 +83,74 @@ public class BDIAgent extends ForesterAgent{
 
 	@Override
 	public void doRequests() {
+		
+		//check dismiss (if other agent changed intention) Other dismiss means your intention is obsolete
+		LinkedList<RequestDismiss> handeled = new LinkedList<RequestDismiss>();
+		for(RequestDismiss rd: this.rejections){
+			Request request = this.requestedAgents.get(rd.getSenderID());
+			if(request.getId() == rd.getRequestID()){
+				this.requestedAgents.remove(rd.getSenderID());
+				handeled.add(rd);
+			}
+		}
+		this.rejections.removeAll(handeled);
+		
+		//check if you need help
 		GridPoint location = grid.getLocation(this);
 		
 		GridCellNgh<Fire> nghFire = new GridCellNgh<>(grid, location,
 				Fire.class, 1, 1);
 		List<GridCell<Fire>> fires = nghFire.getNeighborhood(true);
 		
-		GridCellNgh<ForesterAgent> nghAgents = new GridCellNgh<>(grid, location,
-				ForesterAgent.class, 1, 1);
-		List<GridCell<ForesterAgent>>  agents = nghAgents.getNeighborhood(true);
-		
-		//there are more fires than agents -> call 911
-		if(fires.size()>agents.size()-1){
-			communicationTool.setSendingRange(100);
-			this.costs+=communicationTool.sendRequest(new ActionRequest(1, location.getX(), location.getY(), new Extinguish(), this.communicationId));
+		for(GridCell<Fire> fire: fires){
+			//my intention
+			if(fire.getPoint().getX() == currentIntention.getxPosition() && currentIntention.getyPosition() == fire.getPoint().getY()){
+				continue;
+			}
+			//agents already requested
+			if(agentRequested(fire.getPoint().getX(), fire.getPoint().getY())){
+				continue;
+			}
+			Request request = null;
+			Integer range = null;
+			//already sent request -> increase range
+			for(Request r: openRequests.keySet()){
+				if(r.getPositionX() == fire.getPoint().getX() && r.getPositionY() == fire.getPoint().getY()){
+					request = r;
+					range = openRequests.get(r);
+					break;
+				}
+			}
+			if(request == null){
+				request = new ActionRequest(1, fire.getPoint().getX(), fire.getPoint().getY(), new Extinguish(), communicationId);
+				range = new Integer(3); // starting range
+				this.openRequests.put(request, range);
+			}
+			else{
+				this.openRequests.put(request, ++range);
+			}
+			
 		}
+	}
+	private boolean agentRequested(int x, int y){
+		for(Request request: requestedAgents.values()){
+			if(request.getPositionX() == x && request.getPositionY()== y){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
 	public void sendAnswers() {
 		for(InformationRequest infoRequest : infoRequests)
 		{
-			//TODO Send information answers
+			Information info = this.belief.getInformation(infoRequest.getPositionX(), infoRequest.getPositionY(), infoRequest.getInformation());
+			if(RunEnvironment.getInstance().getCurrentSchedule().getTickCount() - info.getTimestamp() < 20){ // only "new" information
+				this.costs+=communicationTool.sendInformation(info, infoRequest.getSenderID());
+			}
 		}
+		infoRequests.clear();
 		
 		//Send offer
 		//choose request with lowest distance (for now)
@@ -119,15 +168,13 @@ public class BDIAgent extends ForesterAgent{
 				chosenRequest = actionRequest;
 			}
 		}
-		if(chosenRequest == null)
+		if(chosenRequest != null)
 		{
-			//no request -> no offer
-			return;
+			RequestOffer requestOffer = new RequestOffer(chosenRequest.getSenderID(), 
+					chosenRequest.getId(), lowestDistance, false);
+			this.costs+=communicationTool.sendRequestOffer(chosenRequest.getSenderID(), requestOffer);
 		}
 		
-		RequestOffer requestOffer = new RequestOffer(chosenRequest.getSenderID(), 
-				chosenRequest.getId(), lowestDistance, false);
-		communicationTool.sendRequestOffer(chosenRequest.getSenderID(), requestOffer);
 	}
 	
 	@Override
@@ -138,7 +185,25 @@ public class BDIAgent extends ForesterAgent{
 		}
 		messages.clear();
 		
-		//TODO check offers and send confirmations
+		//choose best offers for each openRequest 
+		HashMap<Integer, RequestOffer> bestOffers = new HashMap<Integer, RequestOffer>();
+		for(RequestOffer offer: this.offers){
+			RequestOffer best = bestOffers.get(offer.getRequestID());
+			Request request = openIDs.get(offer.getRequestID());
+			if(request!=null && !agentRequested(request.getPositionX(), request.getPositionY()) && (best == null || offer.getDistance()<best.getDistance())){
+				bestOffers.put(request.getId(), offer);
+			}
+		}
+		//send confirmations to best offers and add to requested agents
+		for(Entry<Integer, RequestOffer> entry: bestOffers.entrySet()){
+			this.costs+=communicationTool.sendRequestConfirm(entry.getValue().getSenderId(), new RequestConfirm(communicationId, entry.getValue().getRequestID()));
+			requestedAgents.put(entry.getValue().getSenderId(), openIDs.get(entry.getKey()));
+			Request removed = openIDs.remove(entry.getKey());
+			openRequests.remove(removed);
+			
+		}
+		//delete other offers
+		offers.clear();
 	}
 
 	@Override
